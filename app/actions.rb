180f719bf12c2c7e732feefc9ab1baeb0031fc91
carmtsang <carmen.tsang1208@hotmail.com>
2021-02-16 04:30:38 +0000

    create likes, comments route

--- app/actions.rb
+++ app/actions.rb
@@ -72,6 +72,33 @@ post '/finstagram_posts' do
 end
 
 get '/finstagram_posts/:id' do
+    halt(401, erb(:'errors/401')) unless !FinstagramPost.where(id: params[:id]).empty?
+
     @finstagram_post = FinstagramPost.find(params[:id])
     erb(:"finstagram_posts/show")
+end
+
+post '/comments' do
+    text = params[:text]
+    finstagram_post_id = params[:finstagram_post_id]
+
+    comment = Comment.new({ text: text, finstagram_post_id: finstagram_post_id, user_id: current_user.id })
+    comment.save
+
+    redirect(back)
+end
+
+post '/likes' do
+    finstagram_post_id = params[:finstagram_post_id]
+
+    like = Like.new({ finstagram_post_id: finstagram_post_id, user_id: current_user.id })
+    like.save
+
+    redirect(back)
+end
+
+delete '/likes/:id' do
+    like = Like.find(params[:id])
+    like.destroy
+    redirect(back)
 end
